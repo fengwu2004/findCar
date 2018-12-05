33001173ab6185f74d68b5ac05308ecd07efb159
Unknown <fengwu2006@gmail.com>
2018-12-05 10:37:40 +0800

    save

--- src/components/AssistBar.vue
+++ src/components/AssistBar.vue
@@ -32,7 +32,7 @@
 
         if (idrCoreMgr.isAndroid) {
 
-          android.doCarFlash({})
+          window.android.doCarFlash()
         }
         else {
 
@@ -43,7 +43,7 @@
 
         if (idrCoreMgr.isAndroid) {
 
-          android.doCarWhistle({})
+          window.android.doCarWhistle()
         }
         else {
 
